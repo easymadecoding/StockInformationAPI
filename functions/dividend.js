ff1d595482dbe3dad18213f99f5af7bbbf463529
alexstaniea <alex.staniea@gmail.com>
2024-04-17 19:20:17 +0300

    Add Get Percentage Over 2 Dates Of Symbol Endpoint

--- functions/dividend.js
+++ functions/dividend.js
@@ -6,7 +6,7 @@ const app = express()
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('Welcome to the stock dividend API')
+    res.json('Welcome to the stock information API')
 })
 
 
@@ -46,6 +46,51 @@ router.get('/:symbol', (req, res) => {
               } 
 
             res.json(parsedData)
+
+        }).catch(err => console.log(err))
+})
+
+router.get('/value/:symbol/:startDate/:endDate', (req, res) => {
+    
+    const { symbol, startDate, endDate } = req.params;
+
+    const url = 'https://finance.yahoo.com/quote/' + symbol + '/history/?period1=' + dateToUnixTimestamp(startDate) +'&period2=' + dateToUnixTimestamp(endDate);
+
+
+    axios.get(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            var startDateValue = 0;
+            var endDateValue = 0;
+            var result = 0.0;
+
+
+            $('tbody tr.svelte-ta1t6m').each((index, element) => {
+                const row = $(element);
+            
+                const dateCell = row.find('td:nth-child(1)').text();
+
+                if (dateCell == startDate) {
+
+                    startDateValue = row.find('td:nth-child(2)').text();
+                    console.log('Start Date Value:', startDateValue);
+                }
+
+                if (dateCell === endDate) {
+                    endDateValue = parseFloat(row.find('td:nth-child(2)').text());
+                    console.log('End Date Value:', endDateValue);
+            
+                    if (startDateValue !== undefined) {
+                        result = ((endDateValue - startDateValue) / startDateValue) * 100;
+                        console.log('Percentage Return:', result.toFixed(2) + '%');
+                    } else {
+                        console.log('Start date not found.');
+                    }
+                }
+            });
+
+            res.json(result)
 
         }).catch(err => console.log(err))
 })
