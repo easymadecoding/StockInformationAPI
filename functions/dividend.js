db23c746e905a9fbd34225eaa78029a9f9640f5c
alexstaniea <alex.staniea@gmail.com>
2024-04-17 19:49:11 +0300

    hopefully this works

--- functions/dividend.js
+++ functions/dividend.js
@@ -22,47 +22,6 @@ router.get('/', (req, res) => {
     res.json('Welcome to the stock information API')
 })
 
-
-router.get('/:symbol', (req, res) => {
-    
-    const symbol = req.params.symbol
-
-    const url = 'https://www.streetinsider.com/dividend_history.php?q='
-
-
-    axios.get(url + symbol)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const parsedData = []
-            const dividendData = []
-
-            $('td', html).each(function () {
-                const data = $(this).text()
-
-                dividendData.push({
-                    data
-                })
-            })
-
-            for (i = 0; i < dividendData.length; i=i+9) {
-
-                var dividend = {
-                    ExDivDate: JSON.stringify(dividendData[i]).split(':')[1].slice(1,-2),
-                    Amount: JSON.stringify(dividendData[i+1]).split(':')[1].slice(1,-2),
-                    DeclarationDate: JSON.stringify(dividendData[i+5]).split(':')[1].slice(1,-2),
-                    RecordDate: JSON.stringify(dividendData[i+6]).split(':')[1].slice(1,-2),
-                    PaymentDate: JSON.stringify(dividendData[i+7]).split(':')[1].slice(1,-2),
-                }
-
-                parsedData.push(dividend)
-              } 
-
-            res.json(parsedData)
-
-        }).catch(err => console.log(err))
-})
-
 router.get('/:symbol/:startDate/:endDate', async (req, res) => {
     
     try{
@@ -116,6 +75,47 @@ router.get('/:symbol/:startDate/:endDate', async (req, res) => {
         console.error('Error:', error);
         res.status(500).send('Internal Server Error');
     }
+})
+
+
+router.get('/:symbol', (req, res) => {
+    
+    const symbol = req.params.symbol
+
+    const url = 'https://www.streetinsider.com/dividend_history.php?q='
+
+
+    axios.get(url + symbol)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const parsedData = []
+            const dividendData = []
+
+            $('td', html).each(function () {
+                const data = $(this).text()
+
+                dividendData.push({
+                    data
+                })
+            })
+
+            for (i = 0; i < dividendData.length; i=i+9) {
+
+                var dividend = {
+                    ExDivDate: JSON.stringify(dividendData[i]).split(':')[1].slice(1,-2),
+                    Amount: JSON.stringify(dividendData[i+1]).split(':')[1].slice(1,-2),
+                    DeclarationDate: JSON.stringify(dividendData[i+5]).split(':')[1].slice(1,-2),
+                    RecordDate: JSON.stringify(dividendData[i+6]).split(':')[1].slice(1,-2),
+                    PaymentDate: JSON.stringify(dividendData[i+7]).split(':')[1].slice(1,-2),
+                }
+
+                parsedData.push(dividend)
+              } 
+
+            res.json(parsedData)
+
+        }).catch(err => console.log(err))
 })
 
 app.use('/.netlify/functions/dividend', router)
