65084f49fa83efadb88c337f28b4bb7b27941fe3
alexstaniea <alex.staniea@gmail.com>
2024-04-18 18:53:12 +0300

    allow cors

--- functions/stockinformation.js
+++ functions/stockinformation.js
@@ -127,14 +127,25 @@ router.get('/dividend/:symbol', (req, res) => {
         }).catch(err => console.log(err))
 })
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+  
+
 app.use('/.netlify/functions/stockinformation', router)
+
+
 
 module.exports.handler=serverless(app)
 
 //remove commented code from below for local testing
 //module.exports = router;
 
-//TO-DO: 
-
-//1. Refactor
-//2. Handle weekend cases
